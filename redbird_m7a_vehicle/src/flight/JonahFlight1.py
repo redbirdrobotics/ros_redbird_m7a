0286ccd8ed6151acf84b5f3097e4a9fc76be97ec
JonahLargen <jhlargen2@gmail.com>
2017-07-20 19:18:30 -0400

    push1

--- redbird_m7a_vehicle/src/flight/JonahFlight1.py
+++ redbird_m7a_vehicle/src/flight/JonahFlight1.py
@@ -11,50 +11,67 @@ from flightsys import Controller, Control_Mode
 __author__ = "Alex Bennett"
 __email__ = "alex.eugene.bennett@gmail.com"
 
+random.seed()
 
-class Test_Flight(flightsys.Flight, object):
+class JonahFlight1(flightsys.Flight, object):
     def __init__(self, vehicle):
         # Call super constructor
-        super(Test_Flight, self).__init__(name='test_flight', log_tag='TEST FLIGHT', vehicle=vehicle)
+        super(JonahFlight1, self).__init__(name='jonah_flight1', log_tag='Jonah Flight 1', vehicle=vehicle)
 
     def flight(self):
     
         x = 0.0
         y = 0.0
         z = 2.5
-        rotations = 4;
+        rotations = 5;
         
         # Takeoff
-        self.takeoff(z)
-
-        # Hold
-        self.loginfo("Altitude goal met! Holding for 2 seconds...")
-        self.sleep(2.0)
+        self.takeoff(z-2.0)
 
         # Fly to point
-        self.fly_to_point((x, y, z))
+        self.fly_to_point((x, y, z), 0.2)
+        
+        # Hold
+        self.loginfo("Altitude goal met at center! Holding for 2 seconds...")
+        self.sleep(2.0)
         
         #Generate random position [pretend robot position in future]
-        x = random.uniform(-10.0, 10.0)
-        y = random.uniform(-10.0, 10.0)
+        x = random.uniform(-3.0, 3.0)
+        y = random.uniform(-3.0, 3.0)
+        self.loginfo("Coordinates= X: " + str(x) + " - Y: + " + str(y) + " - Z: + " + str(z))
+        
+        # Fly to point
+        self.fly_to_point((x, y, z), 0.2)
         
         # Hold
         self.loginfo("Altitude goal met! Holding for 3 seconds...")
         self.sleep(3.0)
 
         # Fly to point [ground]
-        z = 0.5
-        self.fly_to_point((x, y, z))
+        z = 1.0
+        self.fly_to_point((x, y, z), 0.2)
         
         # Rotate robot
         while (rotations > 0):
-            self.fly_to_point((x, y, z + 0.25))
-            self.fly_to_point((x, y, z))
+            self.fly_to_point((x, y, z + 0.2), 0.05)
+            self.loginfo("Coordinates= X: " + str(x) + " - Y: + " + str(y) + " - Z: + " + str(z))
+            self.loginfo("Altitude goal met! Holding for 0.5 seconds...")
+            self.sleep(0.5)
+            self.fly_to_point((x, y, z), 0.05)
+            self.loginfo("Coordinates= X: " + str(x) + " - Y: + " + str(y) + " - Z: + " + str(z))
+            self.loginfo("Altitude goal met! Holding for 0.5 seconds...")
+            self.sleep(0.5)
             rotations-=1
 
         # Fly to point [sky]
+        x = 0.0
+        y = 0.0
         z = 2.5
-        self.fly_to_point((x, y, z))
+        self.fly_to_point((x, y, z), 0.2)
+        
+        
+        #Fly to point [downward]
+        self.fly_to_point((x, y, z-2.0), 0.2)
 
         # Land
         self.land()
--- redbird_m7a_vehicle/src/flight/utils/__init__.py
+++ redbird_m7a_vehicle/src/flight/utils/__init__.py
@@ -1,13 +1,13 @@
 from Configuration import Configuration
 from Logger import Logger
 
-def is_near(coord1, coord2, allowed_range=0.25):
+def is_near(coord1, coord2, allowed_error):
     """Returns true if the supplied cartesian coordinates are within the specified range.
 
     Args:
         coord1 (tuple): Tuple of first coodinate set in the form (X, Y, Z).
         coord2 (tuple): Tuple of second coordinate set in the form (X, Y, Z).
     """
-    return abs(coord1[0] - coord2[0]) < allowed_range and \
-    abs(coord1[1] - coord2[1]) < allowed_range and \
-    abs(coord1[2] - coord2[2]) < allowed_range
+    return abs(coord1[0] - coord2[0]) < allowed_error and \
+    abs(coord1[1] - coord2[1]) < allowed_error and \
+    abs(coord1[2] - coord2[2]) < allowed_error
