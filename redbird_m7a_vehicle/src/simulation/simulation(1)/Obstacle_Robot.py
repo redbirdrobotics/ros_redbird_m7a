9bd49c1a22ba0fd3e05701cbc70dbbd01ba1a860
willbCrawford <wibourecrawford@gmail.com>
2017-07-22 00:50:42 -0400

    simulation(1) will run with localization and simulation(2) will run without localization

---
+++ redbird_m7a_vehicle/src/simulation/simulation(1)/Obstacle_Robot.py
@@ -0,0 +1,112 @@
+from Ground_RobotInterface import Ground_Robot_Interface, iterations
+from Sim_Timer import Sim_Timer
+from math import sqrt, pow, cos, sin
+from time import sleep
+from threading import Thread
+
+class Obstacle_Robot(Ground_Robot_Interface):
+    def __init__(self, x, y, delta_x, delta_y, id, color, timer):
+        #allowing for the timer to be synched with program
+        self._timer = timer
+
+        #getting class variables
+        return super(Obstacle_Robot, self).__init__(x, y, delta_x, delta_y, id, color)
+        
+        #the radius of travel for the obstacle robot
+        self.radius = 1.5 #m
+        
+        #finding the delta angle of travel
+        self._omega = ( ( (sqrt( ( pow(self._deltaX, 2) + pow(self._deltaY, 2) ) ) ) / self.radius) * iterations)
+
+    def update_posX(self):
+        #finding the change in x of travel
+        self._deltaX = cos(self._omega) * self.radius
+
+        self._x = self._x + self._deltaX
+
+    def update_posY(self):
+        #finding the the change in y of travel
+        self._deltaY = sin(self._omega) * self.radius
+
+        self._y = self._y + self._deltaY
+
+    def update_movement(self):
+        #only allwing the code to work if the timer is not paused
+        while not (self._timer._PAUSED.is_set()):
+            #as long as the change in time is no less than 20 than the code will work
+            self.start_timer = self._timer.get_current_timer()
+
+            while self.deltaTime <= 20:
+                #starting the timer
+
+                #setting the currrent position and printing
+                self.current_pos = (self._x, self._y , self._id)
+                print self.current_pos
+
+                self.update_posX()
+                self.update_posY()
+
+                #making sure the angle is doubled
+                self._omega = self._omega * 2 
+
+                self.deltaTime = self._timer.get_current_timer() - self.start_timer
+
+                sleep(iterations)
+            
+            #resetting the timer
+            self.deltaTime = 0
+
+    def run(self):
+        #creating the thread and making the target update movement
+        self._ORThread = Thread(target = self.update_movement)
+
+        try:
+            self._ORThread.start()
+
+            print "Thread Started"
+
+        except:
+
+            print "Thread could not start"
+
+    def get_x(self):
+        return self._x
+
+    def get_y(self):
+        return self._y
+
+    def get_id(self):
+        return self._id
+
+    def change_X_data(self, x):
+        self._x = x
+
+    def change_Y_data(self, y):
+        self._y = y
+
+    def change_VX_data(self, velocityX):
+        self._deltaX = velocityX
+
+    def change_VY_data(self, velocityY):
+        self._deltaY = velocityY
+
+    def check_error(self, x, y, velocityX, velocityY):
+        pErrorX = ((self._x - x) /  x)
+        
+        if not (pErrorX <= 0.001):
+            self.change_X_data(x)
+
+        pErrorY = ((self._y - y) /  y) 
+
+        if not (pErrorY <= 0.001):
+            self.change_Y_data(y)
+
+        pErrorVX = ((self._deltaX - velocityX) / velocityX)
+
+        if not (pErrorVX <= 0.001):
+            self.change_VX_data(velocityX)
+
+        pErrorVY = ((self._deltaY - velocityY) / velocityY)
+
+        if not (pErrorVY<= 0.001):
+            self.change_VY_data(velocityY)
---
+++ redbird_m7a_vehicle/src/simulation/simulation(1)/Target_Robot.py
@@ -0,0 +1,189 @@
+from Ground_RobotInterface import Ground_Robot_Interface, iterations
+from Sim_Timer import Sim_Timer
+from Obstacle_Robot import Obstacle_Robot
+from time import sleep
+from threading import Thread
+from math import fabs, sqrt, tan
+
+class Target_Robot(Ground_Robot_Interface):
+    """description of class"""
+
+    def __init__(self, x, y, deltax, deltay, id, color, timer): 
+        self._timer = timer
+
+        return super(Target_Robot, self).__init__(x, y, deltax, deltay, id, color)
+        
+    def update_posX(self):
+        changeX = self._deltaX * iterations
+        self._x = changeX + self._x
+        return self._x
+
+    def update_posY(self):
+        changeY = self._deltaY * iterations
+        self._y = changeY + self._y
+        return self._y
+     
+    def update_movement(self):
+
+        #adds the ability to pause and resume timer
+        while not (self._timer._quit.is_set()):
+
+            #while the paused flag is not raised
+            while not (self._timer._PAUSED.is_set()):
+                self.start_timer = self._timer.get_current_timer()
+
+                #as long as the time elapsed is less than 20
+                while self.deltaTime < 20:
+
+                    if(self.collision == True):
+                        
+                        self._deltaX = self._deltaX * -1
+
+                        self._deltaY = self._deltaY * -1 
+
+                        self.collision = False
+
+                    else:
+
+                        self.update_posX() 
+                        self.update_posY()
+
+                    self.deltaTime = self._timer.get_current_timer() - self.start_timer
+
+                    sleep(iterations)
+
+                if(self.deltaTime == 20):
+
+                    self.deltaTIme = 0
+
+                    self._timerUp = True
+
+                    super(Target_Robot, self).new_direction()
+
+    def oR_check_collisions(self, obstacle_robots = [Obstacle_Robot]):
+        #making the obstacle robots an array of Obstacle Robots (arbitrary definition)
+
+        #as long as the timer flag is not set
+        while not self._timer._PAUSED.is_set():
+
+            #looping through the list of Obstacle Robots
+            for oRobot in obstacle_robots:
+                
+                #finding the distance from center to center
+                dXX = fabs((oRobot.get_x()) - self._x)
+                dYY = fabs((oRobot.get_y()) - self._y)
+                dCC = sqrt((pow(dXX, 2)) + pow(dYY, 2))
+
+                #creating the max distance the robots can be
+                max_distance = (self._radius + oRobot._radius)
+
+                if dCC <= max_distance:
+
+                    #creating two vectors between the x and y positions
+                    vector_i = oRobot.get_x() - self._x
+                    vector_j = oRobot.get_y() - self._y
+                    
+                    #determining the vector between the two robots and bounds checking the angle
+                    if(vector_i == 0):
+
+                        if(vector_j < 0):
+
+                            theta = 270
+
+                        theta = 90
+
+                    else:
+
+                        theta = tan(vector_j / vector_i)
+
+                    #finding the relative angle needed for the robot's button to be pushed
+                    min_theta = self.get_theta() - 70
+                    max_theta = self.get_theta() + 70
+
+                    if(theta >= min_theta and theta <= max_theta):
+                        self.collision = True
+
+                sleep(iterations/2)
+
+    def run(self, obstacle_robots):
+        self._distanceThread = Thread(target = self.update_movement)
+
+        self._or_collision_thread = Thread(target = self.oR_check_collisions, args = (obstacle_robots,))
+
+        start_time = self._timer.get_current_timer()
+        current_time = 0
+
+        try:
+            self._distanceThread.start()
+
+            print("Update movement thread started")
+
+        except:
+
+            print("Thread failed!")
+
+        try:
+            self._or_collision_thread.start()
+
+            print("Obstacle Robot collision detection started")
+
+        except:
+            print("Thread failed!")
+    
+    def change_X_data(self, x):
+        self._x = x
+
+    def change_Y_data(self, y):
+        self._y = y
+
+    def change_VX_data(self, velocityX):
+        self._deltaX = velocityX
+
+    def change_VY_data(self, velocityY):
+        self._deltaY = velocityY
+
+    def check_error(self, x, y, velocityX, velocityY):
+        pErrorX = ((self._x - x) /  x)
+        
+        if not (pErrorX <= 0.001):
+            self.change_X_data(x)
+
+        pErrorY = ((self._y - y) /  y) 
+
+        if not (pErrorY <= 0.001):
+            self.change_Y_data(y)
+
+        pErrorVX = ((self._deltaX - velocityX) / velocityX)
+
+        if not (pErrorVX <= 0.001):
+            self.change_VX_data(velocityX)
+
+        pErrorVY = ((self._deltaY - velocityY) / velocityY)
+
+        if not (pErrorVY<= 0.001):
+            self.change_VY_data(velocityY)
+
+    def update_data(self, x, y, velocityX, velocityY):
+        self.change_X_data(x)
+
+        self.change_Y_data(y)
+        
+        self.change_VX_data(velocityX)
+
+        self.change_VY_data(velocityY)
+
+    def get_id(self):
+        return self._id
+
+    def get_x(self):
+        return self._x
+
+    def get_y(self):
+        return self._y
+
+    def get_radius(self):
+        return self._radius
+
+    def collision_detection(self, ground_robots):
+
+        return super(Target_Robot, self).run(ground_robots)
