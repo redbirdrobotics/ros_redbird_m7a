fb0803d78c68c72c088080ef2cea129b18ede2a4
Alexander Rickert <ARICKERT0003@gmail.com>
2017-07-22 20:56:19 -0400

    added landmark node

---
+++ redbird_m7a_vehicle/src/localization/Landmark_node.py
@@ -0,0 +1,26 @@
+import cv2
+import numpy as np
+from redbird import LandmarkLib
+from redbird import Camera
+
+#INITIALIZERS
+
+#CAMERAS
+cam0 = Camera(1, (1280, 720), 60, (130, 90), (0, 45))
+cam1 = Camera(1, (1280, 720), 60, (130, 90), (180, 45))
+
+#INSTANCES
+redgoal = Landmark()
+greengoal = Landmark()
+landmarkList[redgoal, greengoal]
+
+while True:
+
+    #Get Frame List
+    
+    #Search for Landmarks
+
+    #Publish to Node
+
+    #Show Frame
+    
